2fe24ffd64bce81512aa2f3f990144cc88120842
Fred Blanc <findel@free.fr>
2021-01-31 23:56:35 +0100

    WIP refacto

--- src/Bottles.php
+++ src/Bottles.php
@@ -14,55 +14,56 @@ class Bottles
     public function verses(int $start, int $end): string
     {
         return implode("\n", array_map(
-            fn($index) => $this->verse($index),
+            fn ($index) => $this->verse($index),
             range($start, $end)
         ));
     }
 
     public function verse(int $index): string
     {
+        return ucfirst($this->indexToText($index)) . " of beer on the wall, " .
+        $this->indexToText($index) . " of beer.\n" .
+        $this->action($index) .
+        $this->indexToText($this->nextIndex($index)) . " of beer on the wall.\n";
+    }
+
+    private function indexToText(int $index): string
+    {
         switch ($index) {
-            case 7:
-                return "7 bottles of beer on the wall, " .
-                "7 bottles of beer.\n" .
-                "Take one down and pass it around, " .
-                "1 six-pack of beer on the wall.\n";
-                break;
-
             case 6:
-                return "1 six-pack of beer on the wall, " .
-                "1 six-pack of beer.\n" .
-                "Take one down and pass it around, " .
-                "5 bottles of beer on the wall.\n";
+                return '1 six-pack';
                 break;
-
-            case 2:
-                return "2 bottles of beer on the wall, " .
-                "2 bottles of beer.\n" .
-                "Take one down and pass it around, " .
-                "1 bottle of beer on the wall.\n";
-                break;
-
             case 1:
-                return "1 bottle of beer on the wall, " .
-                "1 bottle of beer.\n" .
-                "Take it down and pass it around, " .
-                "no more bottles of beer on the wall.\n";
+                return '1 bottle';
                 break;
-
             case 0:
-                return "No more bottles of beer on the wall, " .
-                "no more bottles of beer.\n" .
-                "Go to the store and buy some more, " .
-                "99 bottles of beer on the wall.\n";
+                return 'no more bottles';
                 break;
-
             default:
-                return $index . " bottles of beer on the wall, " .
-                $index . " bottles of beer.\n" .
-                "Take one down and pass it around, " .
-                ($index - 1) . " bottles of beer on the wall.\n";
+                return $index . ' bottles';
                 break;
         }
+    }
+
+    private function action(int $index): string
+    {
+        if ($index === 1) {
+            return "Take it down and pass it around, ";
+        }
+
+        if ($index === 0) {
+            return "Go to the store and buy some more, ";
+        }
+
+        return "Take one down and pass it around, ";
+    }
+
+    private function nextIndex(int $index): int
+    {
+        if ($index === 0) {
+            return 99;
+        }
+
+        return $index - 1;
     }
 }
